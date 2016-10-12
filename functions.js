307c57eb0f9136de5064932d645e7ac3a8f7ef5f
Vasana Chiu <Vasana@Vasanas-MacBook-Pro.local>
2016-10-11 21:58:13 -1000

    4 questions left

--- functions.js
+++ functions.js
@@ -48,57 +48,133 @@ Function - youGetTaco
 Write a function that takes a single String parameter called action. If the value passed into our function is eat have the function return the String value EAT TACOS.
 */
 
+function youGetTaco(action){
+  if(action === "eat"){
+    return "EAT TACOS";
+  }
+}
+console.log (youGetTaco("eat"));
 
 
 /*Function - isNumberGreaterThan
-Write a function that takes two variables of type Number called first and second respectively. Return true if the first number is greater than the second.*/
+Write a function that takes two parameters of type Number called first and second respectively. Return true if the first number is greater than the second.*/
 
 
+function isNumberGreaterThan(first, second){
+  if (first > second){
+    return true;
+  }
+}
+console.log (isNumberGreaterThan(9, 5));
 
 /*Function - isTrue
 Write a function that takes in a Boolean value named val and have the function return whether the value is true.*/
 
-
-
+function isTrue(val){
+  if (val === 7){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log (isTrue(7));
 
 /*Function - isFalse
 Write a function that takes in a Boolean value named val and have the function return whether the value is false.*/
 
-
+function isFalse(val2){
+  if (val2 > 5){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log (isFalse(3));
 
 
 /*Function - isEqual
 Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are equal otherwise return 'Y U NO MATCH!'.*/
 
+function isEqual(firstWord, secondWord){
+  if (firstWord === secondWord){
+    return "AWWWWRIGHT"
+  }else{
+    return "Y U NO MATCH";
+  }
+}
+console.log (isEqual("oh", "oh no"));
 
 /*Function - isNotEqual
 Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are not equal otherwise return 'Y U MATCH!'.*/
 
+function isNotEqual(firstWord, secondWord){
+  if (firstWord !== secondWord){
+    return false;
+  }else{
+    return true;
+  }
+}
+console.log (isNotEqual("yes" , "no"));
 
 /*Function - doubleEquals
 Write a function that takes two variables of type Boolean called first and second respectively. Return true if both values are true otherwise return false.*/
 
-
+function doubleEquals(first, second){
+  if ((first === true) && (second === true)){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log (doubleEquals(true, true));
 
 /*Function - totalOver30
 Write a function that takes three variables of type Number called first and second and third respectively. Return true if the sum of all values are greater than 30 otherwise return false.*/
 
+function totalOver30(first, second, third){
+if (first + second + third > 30){
+  return true;
+}else{
+  return false;
+}
+}
+console.log (totalOver30(10, 4, 29));
 
 
 /*Function - totalUnderWhat
 Write a function that takes four variables of type Number called first and second and third and fourth respectively. Return true if the sum of first, second and third are less than fourth otherwise return false.*/
 
-
+function totalUnderWhat(first, second, third, fourth){
+  if ((first + second + third) < fourth){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log (totalUnderWhat(10, 30, 40, 300));
 
 /*Function - looptoNumber
 Write a function that takes in a Number value named limit and have the function write a for loop that loops the number of times of limit and console.log each number as the loop executes.*/
 
+function loopToNumber(limit){
+  for (var i = 0; i<limit; i++){
+   console.log (i); 
+ }
+}
+loopToNumber(4);
 
 
 /*Function - showEachValue
 Write a function that takes in a Array value named characters that has a sequence of single character String values and have the function write a for loop that loops for each number of elements in the Array and console.log each character as the loop executes.*/
 
+var letters = ["a", "b" , "c"];
 
+function showEachValue(characters){
+for (var i = 0; i<characters.length; i++){
+   console.log (characters[i]);
+}
+}
+showEachValue((letters));
 
 /*Function - createArrayFromString
 Write a function that takes a single variable of type String called word and write a for loop that creates an Array made up of each character in word except for "A" or "a". We don't want no stinking "A" or "a" in our Array. Note: You will need to use the Array.push() method to complete this function.*/
